7ceaa0c22e1bc320cfcc0bc58ce0ae92a39583bc
Fatih Sevencan <fatihsevencan.dev@gmail.com>
2023-07-02 18:48:58 +0300

    feat(bullet):Implement bullet firing functionality

---
+++ spaceinvaders/Classes/Bullet.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "cocos2d.h"
+
+using namespace cocos2d;
+using namespace std;
+
+class Bullet:public Sprite
+{
+    friend class GameScene;
+
+public:
+    static Bullet* createBullet(cocos2d::Vec2 startPosition, float scale, cocos2d::Scene* scene);
+    void Init(Vec2, float, cocos2d::Scene*);
+
+private:
+    cocos2d::Sprite* bulletSprite;
+    cocos2d::Vec2 startPos;
+    int id;
+    float scale;
+    bool activated;
+    Action *action;
+    void Move(Vec2);
+    void Deactivate(Vec2);
+};
